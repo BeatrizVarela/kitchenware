d9b50a83819cf28e69f0a246558181d1afaddb87
MiguelC_98 <miguel_pinto_correia@hotmail.com>
2021-02-16 09:59:24 +0000

    some packages installed and slight cleanup

---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+
+function data() {
+
+    return ([{
+        "Ingredients":{
+            "Garlic": {
+                "type":"Spice",
+                "number":6,
+                "measure":"Unit"
+            },
+            "Potatoes":{
+                "type":"Potato",
+                "number":12,
+                "measure":"Unit"
+            },
+            "Salt":{
+                "type":"Spice",
+                "number":1,
+                "measure":"Kilograms"
+            },
+            "Carrots":20,
+            "Beef":5,
+        },
+        "Recipes":{
+            "Macaroni and Cheese":{
+                "Ingredients needed":[
+                    "Mac",
+                    "Cheese"
+                ],
+                "Recipe":
+                "Put the Mac in the Cheese and ta da"
+            }
+        }
+    }])
+}
