034818c1d49e061e2adff10674ae0b440e2577a2
JLucas022 <joao.thenaisie.lucas@gmail.com>
2021-03-01 22:25:21 +0000

    João: Tabela de ingredientes

--- src/pages/Ingredients.js
+++ src/pages/Ingredients.js
@@ -6,13 +6,37 @@ const Ingredients = () => {
     <section className='ingredients'>
       <Link to='/'>Back</Link>
 
-      <div></div>
+      <div className='header'>
+        <ul>
+          <li id='header-name'>
+            <p>Name</p>
+          </li>
+          <li id='header-quantity'>
+            <p>Quantity</p>
+          </li>
+          <li id='header-variety'>
+            <p>Variety</p>
+          </li>
+        </ul>
+      </div>
 
       <div className='ingredients-stock'>
         <ul>
-          <li>Garlic</li>
-          <li>Potatos</li>
-          <li>Salt</li>
+          <li>
+            <p id="stock-name">Garlic</p>
+            <p id="stock-quantity">6</p>
+            <p id="stock-variety"></p>
+          </li>
+          <li>
+            <p id="stock-name">Potatoes</p>
+            <p id="stock-quantity">12</p>
+            <p id="stock-variety">Red</p>
+          </li>
+          <li>
+            <p id="stock-name">Salt</p>
+            <p id="stock-quantity">1 Kg</p>
+            <p id="stock-variety">Coarse Salt</p>
+          </li>
         </ul>
       </div>
     </section>
