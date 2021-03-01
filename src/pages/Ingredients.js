73591a6537f7a05a7257787388aac2410c78d6a7
JLucas022 <joao.thenaisie.lucas@gmail.com>
2021-03-01 20:03:55 +0000

    João: Tipo de letra diferente + ingredients-stock

--- src/pages/Ingredients.js
+++ src/pages/Ingredients.js
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import '../styles/Ingredients.scss';
 
 const Ingredients = () => {
-    return(
-        <section className="ingredients">
-            <Link to="/">Back</Link>
+  return (
+    <section className='ingredients'>
+      <Link to='/'>Back</Link>
 
-        </section>
-    )
-}
+      <div></div>
+
+      <div className='ingredients-stock'>
+        <ul>
+          <li>Garlic</li>
+          <li>Potatos</li>
+          <li>Salt</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
 export default Ingredients;
