4c4405df16af772c9bab376b848094dbda40849e
Ana Beatriz Varela <varela.ana.beatriz@gmail.com>
2021-03-03 15:44:29 +0000

    Beatriz: alteração tipos de letra

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,44 @@
-import { GiCookingPot,GiCutLemon,GiChickenOven,GiForkKnifeSpoon,GiShoppingCart } from "react-icons/gi";
-import { Link } from 'react-router-dom'
-import "../styles/HomePage.scss"
+import {
+  GiCookingPot,
+  GiCutLemon,
+  GiChickenOven,
+  GiForkKnifeSpoon,
+  GiShoppingCart,
+} from "react-icons/gi";
+import { Link } from "react-router-dom";
+import "../styles/HomePage.scss";
 
 const HomePage = () => {
+  return (
+    <section className="home-page">
+      <div className="center-buttons">
+        <Link to="/ingredients" id="ingredients">
+          <GiCutLemon />
+          <br />
+          <p>IMGREDIENTS</p>
+        </Link>
+        <Link to="/recipes" id="recipes">
+          <GiChickenOven />
+          <br />
+          <p>RECIPES</p>
+        </Link>
+        <Link to="/tableware" id="tableware">
+          <GiForkKnifeSpoon />
+          <br />
+          <p>TABLEWARE</p>
+        </Link>
+        <Link to="/shopping-alerts" id="shopping-alerts">
+          <GiShoppingCart />
+          <br />
+          <p>SHOPPING ALERTS</p>
+        </Link>
+        <div className="center-logo">
+          <GiCookingPot />
+          <h1>KITCHENWARE</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-
-
-    return(
-        <section className="home-page">
-            <div className="center-buttons">
-                <Link to="/ingredients" id="ingredients"><GiCutLemon /><br /><p>Ingredients</p></Link>
-                <Link to="/recipes" id="recipes"><GiChickenOven /><br /><p>Recipes</p></Link>
-                <Link to="/tableware" id="tableware"><GiForkKnifeSpoon /><br /><p>Tableware</p></Link>
-                <Link to="/shopping-alerts" id="shopping-alerts"><GiShoppingCart /><br /><p>Shopping Alerts</p></Link>
-                <div className="center-logo">
-                    <GiCookingPot />
-                    <h1>Kitchenware</h1>
-                </div>
-            </div>
-        </section>
-    )
-}
-
-export default HomePage
+export default HomePage;
