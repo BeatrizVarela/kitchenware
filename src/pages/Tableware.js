da61335353082e40d423b0cd1d9e07166f167bb7
Ana Beatriz Varela <varela.ana.beatriz@gmail.com>
2021-03-03 15:47:33 +0000

    Beatriz: tableware lets-a-go!

--- src/pages/Tableware.js
+++ src/pages/Tableware.js
@@ -1,12 +1,13 @@
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
+import "../styles/Tableware.scss";
+import { useState } from "react";
+import data from "../data";
 
 const Tableware = () => {
-    return(
-        <section className="tableware">
-            <Link to="/">Back</Link>
-
-        </section>
-    );
-}
+  return (
+    <section className="tableware">
+      <Link to="/">Back</Link>
+    </section>
+  );
+};
 export default Tableware;
