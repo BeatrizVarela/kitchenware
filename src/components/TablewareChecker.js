d7abc5034ccd214d6eb5cbeee91b8eab2e95ae0f
Ana Beatriz Varela <varela.ana.beatriz@gmail.com>
2021-03-08 19:03:06 +0000

    Beatriz: início da criação da funcionalidade Submit e respetivo return

--- src/components/TablewareChecker.js
+++ src/components/TablewareChecker.js
@@ -10,7 +10,52 @@ const TablewareChecker = ({ activeButton, setActiveButton, ingAndRec }) => {
     }
   };
   const ButtonSubmit = () => {
-    var Input = document.getElementById("tableware-question-input-id").value;
+    var Input = document.getElementById("tableware-question-input-id").value; //input integer
+
+    var TablewareNameList = ingAndRec.Tableware.map((tab) => {
+      //lista ordenada de nomes de tableware
+      return tab.Name;
+    });
+
+    var TablewareQuantityList = ingAndRec.Tableware.map((tab) => {
+      //lista ordenada de quantidades de tableware
+      return tab.Quantity;
+    });
+
+    var Checkbox = document.getElementsByClassName("tableware-checkmark"); //literalmente as checkboxes ugh
+    var TablewareValueList = []; //lista ordenada de verdadeiros e falsos
+    var i;
+    for (i = 0; i < Checkbox.length; i++) {
+      TablewareValueList.push(Checkbox[i].checked);
+    }
+
+    var TrueValueList = [];
+    var TrueNameList = [];
+    var TrueQuantityList = [];
+
+    for (i = 0; i < TablewareValueList.length; i++) {
+      if (TablewareValueList[i]) {
+        TrueValueList.push(TablewareValueList[i]);
+        TrueNameList.push(TablewareNameList[i]);
+        TrueQuantityList.push(TablewareQuantityList[i]);
+      }
+    }
+
+    var FinalList = [];
+
+    for (i = 0; i < TrueValueList.length; i++) {
+      if (Input > TrueQuantityList[i]) {
+        FinalList.push(TrueNameList[i]);
+      }
+    }
+
+    console.log(TablewareNameList);
+    console.log(TablewareQuantityList);
+    console.log(TablewareValueList);
+    console.log(TrueValueList);
+    console.log(TrueNameList);
+    console.log(TrueQuantityList);
+    console.log(FinalList);
   };
 
   return (
@@ -37,11 +82,11 @@ const TablewareChecker = ({ activeButton, setActiveButton, ingAndRec }) => {
         <div className="outer-checktable">
           {ingAndRec.Tableware.map((tab) => {
             return (
-              <div className="checktable">
+              <div className="checktable" id="checktable-id">
                 <input
                   type="checkbox"
                   id="tableware-name"
-                  name="tableware-name"
+                  className="tableware-checkmark"
                   value={tab}
                 />
                 <label for="tableware-name"> {tab.Name} </label>
@@ -60,7 +105,7 @@ const TablewareChecker = ({ activeButton, setActiveButton, ingAndRec }) => {
         {/* if tableware >= number of people:
             print("You have enough tableware. Enjoy your meal.")
           else:
-            print("You don't have enough tableware. Maybe buy more? Or don't invite so many people idk.")  */}
+            print("You don't have enough tableware. Maybe more [Tableware.Name].")  */}
       </div>
     </div>
   );
