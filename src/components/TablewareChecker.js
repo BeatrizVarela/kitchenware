9574ae629e66ca0e6f2e62f2ee1c5059b63bba7b
Ana Beatriz Varela <varela.ana.beatriz@gmail.com>
2021-03-13 19:33:11 +0000

    Beatriz: criação de div com mensagem final

--- src/components/TablewareChecker.js
+++ src/components/TablewareChecker.js
@@ -1,6 +1,11 @@
 import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 const TablewareChecker = ({ activeButton, setActiveButton, ingAndRec }) => {
+  const [
+    missingTablewareDictionaries,
+    setMissingTablewareDictionaries,
+  ] = useState([]);
   const CloseButtonPressed = () => {
     if (activeButton) {
       setActiveButton(!activeButton);
@@ -9,29 +14,38 @@ const TablewareChecker = ({ activeButton, setActiveButton, ingAndRec }) => {
         .setAttribute("id", "Hidden");
     }
   };
-  const ButtonSubmit = () => {
-    var Input = document.getElementById("tableware-question-input-id").value; //input integer
+  const CloseButtonPressedFinalMessage = () => {
+    if (activeButton) {
+      setActiveButton(!activeButton);
+      document
+        .getElementsByClassName("final-message")[0]
+        .setAttribute("id", "Hidden");
+    }
+  };
 
-    var TablewareNameList = ingAndRec.Tableware.map((tab) => {
+  const ButtonSubmit = () => {
+    let Input = document.getElementById("tableware-question-input-id").value; //input integer
+
+    let TablewareNameList = ingAndRec.Tableware.map((tab) => {
       //lista ordenada de nomes de tableware
       return tab.Name;
     });
 
-    var TablewareQuantityList = ingAndRec.Tableware.map((tab) => {
+    let TablewareQuantityList = ingAndRec.Tableware.map((tab) => {
       //lista ordenada de quantidades de tableware
       return tab.Quantity;
     });
 
-    var Checkbox = document.getElementsByClassName("tableware-checkmark"); //literalmente as checkboxes ugh
-    var TablewareValueList = []; //lista ordenada de verdadeiros e falsos
-    var i;
+    let Checkbox = document.getElementsByClassName("tableware-checkmark"); //literalmente as checkboxes ugh
+    let TablewareValueList = []; //lista ordenada de verdadeiros e falsos
+    let i;
     for (i = 0; i < Checkbox.length; i++) {
       TablewareValueList.push(Checkbox[i].checked);
     }
 
-    var TrueValueList = [];
-    var TrueNameList = [];
-    var TrueQuantityList = [];
+    let TrueValueList = [];
+    let TrueNameList = [];
+    let TrueQuantityList = [];
 
     for (i = 0; i < TablewareValueList.length; i++) {
       if (TablewareValueList[i]) {
@@ -41,13 +55,33 @@ const TablewareChecker = ({ activeButton, setActiveButton, ingAndRec }) => {
       }
     }
 
-    var FinalList = [];
+    let MissingTablewareName = [];
+    let MissingTablewareQuantity = [];
 
     for (i = 0; i < TrueValueList.length; i++) {
       if (Input > TrueQuantityList[i]) {
-        FinalList.push(TrueNameList[i]);
+        MissingTablewareName.push(TrueNameList[i]);
+        MissingTablewareQuantity.push(TrueQuantityList[i]);
       }
     }
+
+    let TablewareDifference = [];
+
+    for (i = 0; i < MissingTablewareName.length; i++) {
+      TablewareDifference.push(Input - MissingTablewareQuantity[i]);
+    }
+
+    var MissingTablewareDictionaries = [];
+
+    for (i = 0; i < MissingTablewareName.length; i++) {
+      let Dict = {
+        name: MissingTablewareName[i],
+        missingQuantity: TablewareDifference[i],
+      };
+      MissingTablewareDictionaries.push(Dict);
+    }
+
+    setMissingTablewareDictionaries(MissingTablewareDictionaries);
 
     console.log(TablewareNameList);
     console.log(TablewareQuantityList);
@@ -55,7 +89,28 @@ const TablewareChecker = ({ activeButton, setActiveButton, ingAndRec }) => {
     console.log(TrueValueList);
     console.log(TrueNameList);
     console.log(TrueQuantityList);
-    console.log(FinalList);
+    console.log(MissingTablewareName);
+    console.log(MissingTablewareQuantity);
+    console.log(TablewareDifference);
+  };
+
+  const TablewareText = () => {
+    let MissingTablewareDictionaries = [...missingTablewareDictionaries];
+    let i = 0;
+    let Text = "Uh-oh! It appears like you're missing ";
+    let Go = "You have enough tableware. Enjoy your meal!";
+    return MissingTablewareDictionaries.map((item) => {
+      i++;
+      if (i + 1 == MissingTablewareDictionaries.length) {
+        return item.missingQuantity + " " + item.name + " and ";
+      } else if (i + 1 < MissingTablewareDictionaries.length) {
+        return item.missingQuantity + " " + item.name + ", ";
+      } else if (i == MissingTablewareDictionaries.length) {
+        return item.missingQuantity + " " + item.name + ".";
+      } else {
+        return "";
+      }
+    });
   };
 
   return (
@@ -101,11 +156,28 @@ const TablewareChecker = ({ activeButton, setActiveButton, ingAndRec }) => {
         >
           Submit
         </button>
-
-        {/* if tableware >= number of people:
-            print("You have enough tableware. Enjoy your meal.")
-          else:
-            print("You don't have enough tableware. Maybe more [Tableware.Name].")  */}
+      </div>
+      <div className="final-message">
+        <div className="button-close">
+          <button onClick={CloseButtonPressedFinalMessage} id="button-close-id">
+            <AiOutlineClose />
+            <p>Close</p>
+          </button>
+        </div>
+        <div className="something-missing">
+          <p className="something-missing-text">
+            Uh-oh! It appears like you're missing <TablewareText />
+          </p>
+        </div>
+        <div className="nothing-missing">
+          <p className="nothing-missing-text">
+            You have enough tableware. Enjoy your meal!
+          </p>
+          {/* if tableware >= number of people:
+              print("You have enough tableware. Enjoy your meal.")
+            else:
+            print("You don't have enough tableware. Maybe more [Tableware.Name].")*/}
+        </div>
       </div>
     </div>
   );
